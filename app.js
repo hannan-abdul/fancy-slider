13491d96de104e5c76df4200a4ccf79870d8a293
hannan-abdul <shohas563@gmail.com>
2021-02-14 22:43:55 +0600

    again upload

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ const showImages = (images) => {
     div.className = 'col-lg-3 col-md-4 col-xs-6 img-item mb-2';
     div.innerHTML = ` <img class="img-fluid img-thumbnail custom-design" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div);
-    
   })
   toggleSpinner();
 }
